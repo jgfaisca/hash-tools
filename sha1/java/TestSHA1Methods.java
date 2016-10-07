1bb7a80def231eb634a8c9b9d983ab6d48678588
zekaf <jose.faisca@gmail.com>
2016-10-07 20:20:37 +0100

    Update TestSHA1Methods.java

--- sha1/java/TestSHA1Methods.java
+++ sha1/java/TestSHA1Methods.java
@@ -13,7 +13,6 @@ public class TestSHA1Methods {
 		System.out.println();
 		System.out.println(SHA1Methods.fileSHA1("test.txt"));
 		System.out.println();
-		System.out.println(SHA1Methods.urlSHA1("https://raw.githubusercontent.com/zekaf/hash-tools/master/java/test.txt"));
-		System.out.println();
+		System.out.println(SHA1Methods.urlSHA1("https://raw.githubusercontent.com/zekaf/hash-tools/master/sha1/java/test.txt"));
 	}
 }
